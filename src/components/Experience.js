514875a6de10861c4159305c41c447029b2f772d
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2023-02-07 17:29:05 +0400

    completed general info page markup

---
+++ src/components/Experience.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Infos.css";
+
+const Experience = () => {
+    return (
+        <div>asdas</div>
+    )
+}
+
+export default Experience;
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,42 +1,50 @@
 import React from "react";
+import "./Infos.css";
+import arrowLeft from "./assets/arrowLeft.png";
 
 const GeneralInfo = () => {
     return (
-        <div>
-            <header>
-                <h1>ᲞᲘᲠᲐᲓᲘ ᲘᲜᲤᲝ</h1>
-                <p>1/3</p>
+        <div className="general-info container">
+            <header className="general-info header">
+                <img src={arrowLeft} alt="left arrow" id="arrow-left" />
+                <div className="header-wrapper">
+                    <h1 className="header-text">ᲞᲘᲠᲐᲓᲘ ᲘᲜᲤᲝ</h1>
+                    <p>1/3</p>
+                </div>
             </header>
             <form>
-                <div>
-                    <label>სახელი </label>
-                    <input type="text" name="name" />
-                    <p>მინიმუმ 2 ასო, ქართული ასოები</p>
+                <div className="form-wrapper">
+                    <div className="input-wrapper">
+                        <label>სახელი </label>
+                        <input type="text" name="name" />
+                        <p>მინიმუმ 2 ასო, ქართული ასოები</p>
+                    </div>
+                    <div className="input-wrapper">
+                        <label>გვარი </label>
+                        <input type="text" name="lastname" />
+                        <p>მინიმუმ 2 ასო, ქართული ასოები</p>
+                    </div>
                 </div>
-                <div>
-                    <label>გვარი </label>
-                    <input type="text" name="lastname" />
-                    <p>მინიმუმ 2 ასო, ქართული ასოები</p>
+                <div className="pic-wrapper">
+                    <label>პირადი ფოტოს ატვირთვა</label>
+                    <button >ატვირთვა</button>
                 </div>
-                <div>
-                    <p>პირადი ფოტოს ატვირთვა</p>
-                    <button>ატვირთვა</button>
+                <div className="input-wrapper info">
+                    <label id="info-label">ჩემ შესახებ (არასავალდებულო) </label>
+                    <input type="text" name="info" placeholder="ზოგადი ინფო შენ შესახებ" id="info-text" />
                 </div>
-                <div>
-                    <label>ჩემ შესახებ (არასავალდებულო) </label>
-                    <input type="text" name="info" placeholder="ზოგადი ინფო შენ შესახებ" />
-                </div>
-                <div>
-                    <label>ელ.ფოსტა</label>
-                    <input type="email" name="mail" />
+                <div className="input-wrapper contact">
+                    <label id="mail-label">ელ.ფოსტა</label>
+                    <input type="email" name="mail" id="mail-text" />
                     <p>უნდა მთავრდებოდეს @redberry.ge-ით</p>
                 </div>
-                <div>
-                    <label>მობილურის ნომერი</label>
-                    <input type="email" name="number" />
+                <div className="input-wrapper contact">
+                    <label id="phone-label">მობილურის ნომერი</label>
+                    <input type="text" name="number" id="phone-text" />
                     <p>უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს</p>
                 </div>
             </form>
+            <button type="sumbit" className="page-toggle button">ᲨᲔᲛᲓᲔᲒᲘ</button>
         </div>
     )
 }
